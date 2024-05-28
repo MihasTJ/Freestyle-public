9dc61baef62b1a8fc53150aee632d9a5095744ea
MihasTJ <michal.janiol00@gmail.com>
2024-05-28 18:32:31 +0200

    nav

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+    const navSlider = $("nav .slider");
+    const navSliderIcon = $("nav .slider i");
+    const navItems = $("nav .slider-items");
 
+    navSlider.on("click",()=>{
+        navSliderIcon.toggleClass("rotate-180");
+        navItems.fadeToggle(100);
+    })
 });
