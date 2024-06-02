3b54a4a115b2b13fc2cd75457b412192b791ae57
MihasTJ <michal.janiol00@gmail.com>
2024-06-02 22:15:59 +0200

    create #page, fixed nav border

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
-    const navItemsWithOl = $("nav ul > li:has(ol)");
+    /* create and move to right side */
+    $("nav").after("<div id='page'></div>");
+    $("body").children().not("nav, #page,script").appendTo("#page");
 
+    const navItemsWithOl = $("nav ul > li:has(ol)");
     /* fixed nav-list */
     navItemsWithOl.each(function() {
         $(this).addClass("fixed-list");
         $(this).children("a").append(' <i class="fa-solid fa-chevron-down"></i>');
     });
 
+    /* node list */
     const navFlip = $("nav .fixed-list > a");
+    navFlip.siblings("ol").children("li").addClass("border");
     navFlip.each(function() {
         $(this).click(function(event) {
             event.preventDefault();
+            $(this).siblings("ol").children("li").toggleClass("border");
             $(this).siblings("ol").slideToggle();
             const icon = $(this).find(".fa-chevron-down");
             icon.toggleClass("rotate-180");
