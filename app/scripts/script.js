2796386090e6aa0f6b29e876856fafb89cc38cdd
MihasTJ <michal.janiol00@gmail.com>
2024-06-04 20:22:24 +0200

    resize navigation

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -12,25 +12,61 @@ $(document).ready(function(){
 
     /* node list */
     const navFlip = $("nav .fixed-list > a");
-    navFlip.siblings("ol").children("li").addClass("border");
+    navFlip.siblings("ol").children("li").addClass("bordernav");
+    
     navFlip.each(function() {
         $(this).click(function(event) {
-            event.preventDefault();
-            $(this).siblings("ol").children("li").toggleClass("border");
-            $(this).siblings("ol").slideToggle();
-            const icon = $(this).find(".fa-chevron-down");
-            icon.toggleClass("rotate-180");
-            
-            if (icon.hasClass("rotate-180")) {
-                $(this).parent().addClass("fixed-list-padding");
-            } else {
-                $(this).parent().removeClass("fixed-list-padding");
+            if (!$(this).closest(".nav-container").hasClass("panel-menu")) {
+                event.preventDefault();
+                
+                $(this).siblings("ol").children("li").toggleClass("bordernav");
+                $(this).siblings("ol").slideToggle();
+                
+                const icon = $(this).find(".fa-chevron-down");
+                icon.toggleClass("rotate-180");
+                
+                if (icon.hasClass("rotate-180")) {
+                    $(this).parent().addClass("fixed-list-padding");
+                } else {
+                    $(this).parent().removeClass("fixed-list-padding");
+                }
             }
         });
     });
 /* burger menu */
     const btnBurger = $("nav .burgerbtn");
-    btnBurger.on('click',()=>{
+    const btnToggleNav = $("nav .toggle-nav");
+
+    function toggleButtonText(button) {
+        if (button.text() === "Rozwiń") {
+            button.text("Zwiń");
+        } else {
+            button.text("Rozwiń");
+        }
+    }
+    function sizeNav(){
+        if($(".nav-container").hasClass("panel-menu")){
+            $(':root').css('--nav-width','50px');
+        }else{
+            $(':root').css('--nav-width','200px');
+        }
+    }
+
+    btnBurger.on('click', function() {
+        $(this).toggleClass("active-menu");
+        btnToggleNav.each(function() {
+            toggleButtonText($(this));
+        });
+        navFlip.closest(".nav-container").toggleClass("panel-menu");
+        sizeNav()
+    });
+
+    btnToggleNav.on('click', function() {
+        btnToggleNav.not(this).text("Rozwiń");
+        toggleButtonText($(this));
         btnBurger.toggleClass("active-menu");
-    })
+        navFlip.closest(".nav-container").toggleClass("panel-menu");
+        sizeNav()
+    });
+
 });
